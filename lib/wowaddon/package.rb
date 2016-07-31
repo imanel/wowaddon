bd938c3d7e63bfa873b79a4bd39a0f8d139da788
Bernard Potocki <bernard.potocki@imanel.org>
2016-07-31 09:22:50 +0200

    Add changelog information

--- lib/wowaddon/package.rb
+++ lib/wowaddon/package.rb
@@ -1,7 +1,17 @@
 require 'active_record'
+require 'oga'
+require 'open-uri'
 
 module Wowaddon
   class Package < ActiveRecord::Base
     establish_connection adapter: 'sqlite3', database: Wowaddon.config.database_file
+
+    def changelog
+      return @changelog if defined? @changelog
+      url = "https://mods.curse.com/addons/wow/" + curse_id
+      file = open url
+      document = Oga.parse_html file
+      @changelog = document.css("#tab-changes").text.strip
+    end
   end
 end
--- wowaddon.gemspec
+++ wowaddon.gemspec
@@ -21,5 +21,6 @@ Gem::Specification.new do |spec|
   spec.add_dependency('activerecord', '~> 5.0')
   spec.add_dependency('bzip2-ffi', '~> 1.0.0')
   spec.add_dependency('gli', '~> 2.14.0')
+  spec.add_dependency('oga')
   spec.add_dependency('sqlite3', '~> 1.3.11')
 end
